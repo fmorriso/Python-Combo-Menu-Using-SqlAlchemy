5963704ebaf9ec523ba6217db46e1d5251b0fb3f
Frederick Morrison <frederick.morrison@outlook.com>
2024-03-09 11:13:00 -0700

    start conversion of MenuItem

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sys
 import sqlalchemy as sa
+
+from menu_item import MenuItem
 from user import User
 
 
@@ -16,8 +18,16 @@ def make_example_user():
     print(f'{u1.__repr__()}')
 
 
+def make_example_menu_item():
+    category = 'Sandwich'
+    for selection in (['Chicken', 5.25], ['Beef', 6.25], ['Tofu', 5.75]):
+        menu_item = MenuItem(category, selection[0], selection[1])
+        print(f'{menu_item=}')
+
+
 if __name__ == '__main__':
     print(f'Combo Menu via SqlAlchemy using python version {get_python_version()}')
     print(f'and SQLAlchemy version {get_SqlAlchemy_version()}')
 
-    make_example_user()
+    # make_example_user()
+    make_example_menu_item()
---
+++ menu_item.py
@@ -0,0 +1,19 @@
+from typing import Optional
+import uuid
+
+from base_table import Base
+
+from sqlalchemy import NVARCHAR, Uuid
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
+
+class MenuItem(Base):
+    __tablename__ = "menu_item"
+
+    id: Mapped[Optional[uuid]] = mapped_column(Uuid, primary_key=True, init=False)
+
+    category: Mapped[str] = mapped_column(NVARCHAR(20), nullable=False)
+    name: Mapped[str] = mapped_column(NVARCHAR(50), nullable=False)
+    price: Mapped[float]
+    quantity: Mapped[Optional[int]] = 1
